08bd4225b010e2badc55bb4acb2b80ab9981628b
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2019-04-24 19:48:00 +0200

    example for numerical integration was refactored

--- examples/numerical_integration.py
+++ examples/numerical_integration.py
@@ -2,14 +2,10 @@
 
 import numpy as np
 from numerical.integration import gauss
-from numerical.area import bounds
-from numerical.area import grid
 
 
 def f(x):
     return np.power(x[0], 2) + 2 * x[1] + 5
 
 
-bd = bounds.cartesian.LineBoundary2D(0, 1, 0, 2 * np.pi)
-grid = grid.UniformGrid(bd, [0.02, np.pi / 8])
-gauss.integrate(f, grid)
+gauss.integrate(f, 0, 1, 0, 2 * np.pi, steps=(0.02, np.pi / 8))
