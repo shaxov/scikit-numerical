05dac15dd22d4d792b62c7050769e9203675e9f2
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2019-04-24 16:49:03 +0200

    castesian boundaries were made integrable

--- numerical/area/bounds/cartesian.py
+++ numerical/area/bounds/cartesian.py
@@ -2,11 +2,16 @@ from numerical.area.bounds import _base
 import matplotlib.pyplot as plt
 
 
-class CartesianCoordinates:
+class _CartesianCoordinates:
     COORDS_TYPE = "cartesian"
 
 
-class LineBoundary1D(_base.BoundaryIterable, CartesianCoordinates):
+_Iterable = _base.BoundaryIterable
+_Integrable = _base.BaseIntegrableBoundary
+_Cartesian = _CartesianCoordinates
+
+
+class LineBoundary1D(_Iterable, _Integrable, _Cartesian):
     def __init__(self, x_start, x_end):
         super().__init__(
             [
@@ -16,16 +21,16 @@ class LineBoundary1D(_base.BoundaryIterable, CartesianCoordinates):
         self._set_described_rect()
 
     def _set_described_rect(self):
-        self._described_rect = self._data
+        self._described_rect = self._bounds
 
     def get_described_rect(self):
         return self._described_rect
 
     def plot(self, save_path=None):
-        self._data[0].plot(save_path)
+        self._bounds[0].plot(save_path)
 
 
-class LineBoundary2D(_base.BoundaryIterable, CartesianCoordinates):
+class LineBoundary2D(_Iterable, _Integrable, _Cartesian):
     def __init__(self,
                  x_start, x_end,
                  y_start, y_end):
@@ -38,14 +43,14 @@ class LineBoundary2D(_base.BoundaryIterable, CartesianCoordinates):
         self._set_described_rect()
 
     def _set_described_rect(self):
-        self._described_rect = self._data
+        self._described_rect = self._bounds
 
     def get_described_rect(self):
         return self._described_rect
 
     def plot(self, save_path=None):
-        x_start, x_end = self._data[0].start, self._data[0].end
-        y_start, y_end = self._data[1].start, self._data[1].end
+        x_start, x_end = self._bounds[0].start, self._bounds[0].end
+        y_start, y_end = self._bounds[1].start, self._bounds[1].end
         x_start2, x_end2 = self._described_rect[0].start, self._described_rect[0].end
         y_start2, y_end2 = self._described_rect[1].start, self._described_rect[1].end
 
@@ -62,7 +67,7 @@ class LineBoundary2D(_base.BoundaryIterable, CartesianCoordinates):
             plt.show()
 
 
-class LineBoundary3D(_base.BoundaryIterable, CartesianCoordinates):
+class LineBoundary3D(_Iterable, _Integrable, _Cartesian):
     def __init__(self,
                  x_start, x_end,
                  y_start, y_end,
@@ -77,7 +82,7 @@ class LineBoundary3D(_base.BoundaryIterable, CartesianCoordinates):
         self._set_described_rect()
 
     def _set_described_rect(self):
-        self._described_rect = self._data
+        self._described_rect = self._bounds
 
     def get_described_rect(self):
         return self._described_rect
