555f5bb7cbd4a2a4df876c9cada067ea66fbe7bc
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2019-04-24 21:36:23 +0200

    version was chenged

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scikit-numerical",
-    version="0.0.7",
+    version="0.0.8",
     author="Maksym Shpakovych",
     author_email="maksym.shpakovych@gmail.com",
     description="Tools for numerical math calculations",
