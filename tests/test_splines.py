0b928bf09129b311697d1cbacc8f4b61dee3277c
Maksym Shpakovych <maksym.shpakovych@inria.fr>
2019-04-25 13:43:48 +0200

    tests for derivatives were added

--- tests/test_splines.py
+++ tests/test_splines.py
@@ -12,6 +12,11 @@ class SplinesTest(unittest.TestCase):
 
     def test_shenberg_spline(self):
         var = np.array([-1.52, 0.001, -0.001, 0.0, 2.356, 1.254, 3.260, 3.0])
-        self.assertTrue(np.allclose(splines.shenberg(var),
+        self.assertTrue(np.allclose(splines.schoenberg(var),
                         np.array([0, 0.5499995, 0, 0.55, 0.0009231, 0.1236675, 0., 0.])))
-
+        self.assertTrue(np.allclose(splines.schoenberg.deriv(var, order=1),
+                                    [0., -0.001, 0., 0., -0.00716691, -0.30979956, 0., 0.]))
+        self.assertTrue(np.allclose(splines.schoenberg.deriv(var, order=2),
+                                    [0., -0.999997, 0., -1., 0.044515, 0.47195722, 0., 0.]))
+        self.assertTrue(np.allclose(splines.schoenberg.deriv(var, order=3),
+                                    [0., 0.005995, 0., 0., -0.207368, 0.14529, 0., 0.]))
