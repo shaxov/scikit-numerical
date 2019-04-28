e1b1687c1b5a60c941c8771eb83ba14aef1b84c5
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2019-04-28 14:47:11 +0200

    tests were fixed

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -6,23 +6,23 @@ from numerical import interpolate
 class InterpolationTest(unittest.TestCase):
 
     def test_interpolation(self):
-        def fun(x):
+        def fun1d(x):
             return np.power(x[0], 2) + 4
 
         meshgrid = [np.arange(0, 1.000001, 0.001)]
-        values = fun(meshgrid)
+        values = fun1d(meshgrid)
 
         spline_fun = interpolate(values, meshgrid, 1)
         x = np.random.rand(1, 10)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun1d(x), atol=1e-2, rtol=1e-2))
 
         x = np.random.rand(1, 10, 10)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun1d(x), atol=1e-2, rtol=1e-2))
 
         x = np.random.rand(1, 10, 10, 10)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun1d(x), atol=1e-2, rtol=1e-2))
 
-        def fun(x):
+        def fun2d(x):
             return np.power(x[0], 3) * x[1] + 7 * x[1] + np.power(x[0], 0.5)
 
         grid1 = np.arange(0, 1.0001, 0.01)
@@ -33,15 +33,15 @@ class InterpolationTest(unittest.TestCase):
         spline_fun = interpolate(values, meshgrid, 1)
 
         x = np.random.rand(2, 5)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun2d(x), atol=1e-2, rtol=1e-2))
 
         x = np.random.rand(2, 5, 5)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun2d(x), atol=1e-2, rtol=1e-2))
 
         x = np.random.rand(2, 5, 5, 5)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun2d(x), atol=1e-2, rtol=1e-2))
 
-        def fun(x):
+        def fun3d(x):
             return (7 * x[0] * x[2] - np.power(x[1], 2)) * x[2]
 
         grid1 = np.arange(0, 1.0001, 0.01)
@@ -54,10 +54,10 @@ class InterpolationTest(unittest.TestCase):
         spline_fun = interpolate(values, meshgrid, 1)
 
         x = np.random.rand(3, 5)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun3d(x), atol=1e-2, rtol=1e-2))
 
         x = np.random.rand(3, 5, 5)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun3d(x), atol=1e-2, rtol=1e-2))
 
         x = np.random.rand(3, 5, 5, 5)
-        self.assertTrue(np.allclose(spline_fun(x), fun(x), atol=1e-2, rtol=1e-2))
+        self.assertTrue(np.allclose(spline_fun(x), fun3d(x), atol=1e-2, rtol=1e-2))
