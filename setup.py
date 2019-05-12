593a22a94b19affb119dff2633bcc631f5036307
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2019-05-12 11:28:52 +0200

    Version is changed: 0.0.8 -> 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scikit-numerical",
-    version="0.0.8",
+    version="0.0.9",
     author="Maksym Shpakovych",
     author_email="maksym.shpakovych@gmail.com",
     description="Tools for numerical math calculations",
